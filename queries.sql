e9598a1cf2fc419a5ea34c5106b24eac0596a4b9
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-05-09 08:59:04 +0530

    Update Record

--- queries.sql
+++ queries.sql
@@ -9,3 +9,51 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name <> 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon' AND species <> 'digimon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+SELECT * FROM animals;
+COMMIT;
+SELECT * FROM animals;
+
+
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+SELECT COUNT(*) FROM animals;
+
+
+-- Delete animals born after Jan 1st, 2022, savepoint, update weight, rollback, update negative weights, commit
+BEGIN;
+    DELETE FROM animals WHERE data_of_birth > '2022-01-01';
+    SAVEPOINT my_savepoint;
+    UPDATE animals SET weight_kg = weight_kg * -1;
+    ROLLBACK TO my_savepoint;
+    UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
+
+SELECT * FROM animals;
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT neutered, AVG(escape_attempts) as avg_attempts 
+FROM animals 
+GROUP BY neutered 
+ORDER BY avg_attempts DESC 
+LIMIT 1;
+SELECT species, MIN(weight_kg) as min_weight, MAX(weight_kg) as max_weight 
+FROM animals 
+GROUP BY species;
+SELECT species, AVG(escape_attempts) as avg_attempts 
+FROM animals 
+WHERE data_of_birth BETWEEN '1990-01-01' AND '2000-12-31' 
+GROUP BY species;
+
